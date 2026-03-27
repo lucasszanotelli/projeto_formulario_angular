50beae6b26583f8613f602f0b06362642146a0fb
Lucas Zanotelli <lucasszanotelli@gmail.com>
2026-03-27 17:28:48 -0300

    projeto formulário atv II dv WEB II

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Formulario } from './formulario/formulario';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Formulario},
+];
